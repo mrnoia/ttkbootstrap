b1e7c4734a5807d96c1c47956b3aea48f6dd6033
mrnoia <emanuelenoia@hotmail.com>
2023-01-23 20:29:26 +0000

    Hello World

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ root = tb.Window(themename="superhero")
 root.title("TTKBootstrap")
 # root.iconbitmap("icon.ico")
 root.geometry("500x500")
+counter = 0
+
+
+def changer():
+    global counter
+    counter += 1
+
+    if counter % 2 == 0:
+        my_label.config(text="Hello World")
+    else:
+        my_label.config(text="Bye World")
+
 
 my_label = tb.Label(root, text="Hello World", font=(
     "Helvetica", 18), bootstyle="primary")
 my_label.pack(pady=50)
+mybutton = tb.Button(root, text="Click Me",
+                     bootstyle="success,outline", command=changer)
+mybutton.pack(pady=10)
 root.mainloop()
