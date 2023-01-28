3769144b75ee24ae649b6838315adee859ce99c1
mrnoia <emanuelenoia@hotmail.com>
2023-01-28 14:36:46 +0000

    experimenting with grid

--- table_view.py
+++ table_view.py
@@ -6,9 +6,9 @@ app = ttk.Window()
 colors = app.style.colors
 
 coldata = [
-    {"text": "LicenseNumber", "stretch": False},
-    "CompanyName",
-    {"text": "UserCount", "stretch": False},
+    {"text": "LicenseNumber", "stretch": False, "width": 1000},
+    {"text": "CompanyName", "stretch": False},
+    {"text": "UserCount", "stretch": True},
 ]
 
 rowdata = [
@@ -17,20 +17,18 @@ rowdata = [
     ("A158", "Farmadding Co.", 36),
 ]
 
+
 dt = Tableview(
     master=app,
     coldata=coldata,
     rowdata=rowdata,
     paginated=True,
     searchable=True,
-    bootstyle=PRIMARY,
-    stripecolor=(colors.light, None),
+    bootstyle=INFO,
+    stripecolor=('#ffe6e6', None),
+
+
 )
+
 dt.pack(fill=BOTH, expand=YES, padx=10, pady=10)
-# make the table expand to fit the window when it is expanded
-app.rowconfigure(0, weight=1)
-app.columnconfigure(0, weight=1)
-
-
-dt.autofit_columns()
 app.mainloop()
